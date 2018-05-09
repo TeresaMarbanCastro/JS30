7962379fb4f92d072365a95be9c769a252d732f6
usuario <teresamarbancastro@gmail.com>
2018-05-09 17:39:43 +0200

    DRUMKIT done

--- drumKit/main.js
+++ drumKit/main.js
@@ -1,6 +1,20 @@
-(function (){
+
 'use strict'
+function playSound(e){
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if(!audio) return; //stop the function from running all together
+    audio.currentTime = 0; //rewind to start
+    audio.play();
+    key.classList.add('playing');
 
+});
 
-
-})()
+function removeTransition(e){
+    if (e.propertyName !== 'transform') return;
+    // console.log(this);  //it equals what gets called
+    this.classList.remove('playing');
+}
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown',playSound);
