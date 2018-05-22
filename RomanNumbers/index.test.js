3d56e2754c28b691e1b7425b0fa933df3563864e
Teresa <teresamarbancastro@gmail.com>
2018-05-22 15:28:11 +0200

    node modules

--- RomanNumbers/index.test.js
+++ RomanNumbers/index.test.js
@@ -1,9 +1,9 @@
 const application = require('./main');
 
-​describe('the application', () =>{
+​describe('the application', function(){
 
     test('adds 1 + 2 to equal 3', () => {
-    expect(application(1, 2)).toBe(3);
+        expect(application(1, 2)).toBe(3);
     });
     
 });
