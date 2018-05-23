21b48ec007cc7dd89c9a6451e25e6c4d043955ab
Teresa <teresamarbancastro@gmail.com>
2018-05-23 12:07:33 +0200

    RomanNumber

--- RomanNumbers/index.test.js
+++ RomanNumbers/index.test.js
@@ -1,12 +1,21 @@
-const intToRoman = require('./main.js');
+const startApp = require('./main.js');
+var app;
 
 describe('Roman Number conversor', function(){
-
+    app = startApp();
     it('must return roman number', function(){
-        expect(intToRoman.fromNumber(1)).toBe('I');
-        expect(intToRoman.fromNumber(2)).toBe('II');
-        expect(intToRoman.fromNumber(3)).toBe('III');
-        expect(intToRoman.fromNumber(4)).toBe('IV');
+        expect(app.init(1)).toBe('I');
+        expect(app.init(2)).toBe('II');
+        expect(app.init(3)).toBe('III');
+        expect(app.init(389)).toBe('CCCLXXXIX');
     });
+    // it('must return roman number', function(){
+    //     expect(intToRoman.fromNumber(1)).toBe('I');
+    //     expect(intToRoman.fromNumber(2)).toBe('II');
+    //     expect(intToRoman.fromNumber(3)).toBe('III');
+    //     expect(intToRoman.fromNumber(4)).toBe('IV');
+    //     expect(intToRoman.fromNumber(2396)).toBe('MMCCCXCVI');
+
+    // });
 });
 
--- RomanNumbers/main.js
+++ RomanNumbers/main.js
@@ -1,36 +1,82 @@
-var intToRoman = function(){};
+//Module pattern implementation
 
-var numeral = function(value, roman){
-    return {
-        value: value,
-        roman: roman
-    };
-};
-
-var numbers = [
-    numeral(1000, 'M'),
-    numeral(900, 'CM'),
-    numeral(500, 'D'),
-    numeral(400, 'CD'),
-    numeral(100, 'C'),
-    numeral(90, 'XC'),
-    numeral(50, 'L'),
-    numeral(40, 'XL'),
-    numeral(10, 'X'),
-    numeral(9, 'IX'),
-    numeral(5, 'V'),
-    numeral(4, 'IV'),
-    numeral(1, 'I') 
-];
-intToRoman.prototype.fromNumber = function(n){
-    var romanNumber = '';
-    numbers.forEach(function(item){
-        for (; n >= item.value; n -= item.value){
-            romanNumber += item.roman;
+var startApp = function () {
+    var numeral = function (value, roman) {
+        return {
+            value: value,
+            roman: roman
         };
-    });
-    return romanNumber;
+    };
+
+    var numbers = [
+        numeral(1000, 'M'),
+        numeral(900, 'CM'),
+        numeral(500, 'D'),
+        numeral(400, 'CD'),
+        numeral(100, 'C'),
+        numeral(90, 'XC'),
+        numeral(50, 'L'),
+        numeral(40, 'XL'),
+        numeral(10, 'X'),
+        numeral(9, 'IX'),
+        numeral(5, 'V'),
+        numeral(4, 'IV'),
+        numeral(1, 'I')
+    ];
+
+    var intToRoman = function (n) {
+        var romanNumber = '';
+        numbers.forEach(function (item) {
+            for (; n >= item.value; n -= item.value) {
+                romanNumber += item.roman;
+            };
+        });
+        return romanNumber;
+    };
+
+    return {
+        init: intToRoman
+    }
 };
 
-module.exports = new intToRoman();
+if (typeof(module) != 'undefined'){
+    module.exports = startApp;
+}
+
+//'Normal' implementation
+// var intToRoman = function(){};
+
+// var numeral = function(value, roman){
+//     return {
+//         value: value,
+//         roman: roman
+//     };
+// };
+
+// var numbers = [
+//     numeral(1000, 'M'),
+//     numeral(900, 'CM'),
+//     numeral(500, 'D'),
+//     numeral(400, 'CD'),
+//     numeral(100, 'C'),
+//     numeral(90, 'XC'),
+//     numeral(50, 'L'),
+//     numeral(40, 'XL'),
+//     numeral(10, 'X'),
+//     numeral(9, 'IX'),
+//     numeral(5, 'V'),
+//     numeral(4, 'IV'),
+//     numeral(1, 'I') 
+// ];
+// intToRoman.prototype.fromNumber = function(n){
+//     var romanNumber = '';
+//     numbers.forEach(function(item){
+//         for (; n >= item.value; n -= item.value){
+//             romanNumber += item.roman;
+//         };
+//     });
+//     return romanNumber;
+// };
+
+// module.exports = new startApp(); Asignar una instancia (NEW)
 
